40ab4857936abc024eb1978ca88ce56055f9e4cf
zaemtariq <zaemtariq@gmail.com>
2025-03-27 06:04:21 +0500

    updated2

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"",
     credentials:true
 }))
 app.use("/api/auth", route);
