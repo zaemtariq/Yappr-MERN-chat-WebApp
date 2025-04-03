7344f10d82448e97f6cca392d54a8a41cc637306
Rana Zaeem <87516819+zaemtariq@users.noreply.github.com>
2025-04-03 23:52:55 +0500

    Update index.js

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -25,10 +25,10 @@ app.use("/api/auth", route);
 app.use("/api/messages", messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../FRONTEND/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../FRONTEND", "dist", "index.html"));
   });
 }
 
