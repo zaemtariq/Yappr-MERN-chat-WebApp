27fa4b381c9a55c413623b67eab879812e97cc7a
Rana Zaeem <87516819+zaemtariq@users.noreply.github.com>
2025-04-03 23:27:55 +0500

    Update axios.js

--- FRONTEND/src/lib/axios.js
+++ FRONTEND/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios  from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE == "development" ? "http://localhost:5001/api" : "https://yappr-mern-chat-web-app.vercel.app/api",
+    baseURL:import.meta.env.MODE == "development" ? "http://localhost:5001/api" : "/api",
     withCredentials:true,
 })  
